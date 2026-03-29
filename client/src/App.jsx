ce6620fec96985d50a4275b16035d9836735f522
Tetiana Fesiuk <fetanik28@gmail.com>
2026-03-30 01:44:02 +0300

    Add Registration page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import RegistrationPage from './pages/RegistrationPage';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          {/* Твоя сторінка тепер відкривається першою (за замовчуванням) */}
+          <Route path="/" element={<RegistrationPage />} />
+          
+          {/* Сторінку Олесі ми тимчасово перемістили сюди, щоб не зламати її роботу */}
+          <Route path="/home" element={<HomePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
