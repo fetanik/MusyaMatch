e347fa4018854f05e2564ac9717c656a09ed593f
Гречанюк Дмитро web2025-1 <dmytro.hrechaniuk@lnu.edu.ua>
2026-04-11 23:00:58 +0300

    feat: implement veterinary pharmacies page with google maps integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RegistrationPage from './pages/RegistrationPage';
+import PharmaciesPage from './pages/PharmaciesPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/pharmacies" element={<PharmaciesPage />} />
         </Routes>
       </div>
     </BrowserRouter>
