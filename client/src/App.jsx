0e3f15170de7787be38034d97be8a99e049501c5
Tetiana Fesiuk <fetanik28@gmail.com>
2026-04-24 01:07:53 +0300

    Add shelter manager dashboard, settings and vaccination pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RegistrationPage from './pages/RegistrationPage';
 import ProfilePage from './pages/ProfilePage';
 import ManagerProfile from './pages/ManagerProfile';
+import ManagerSettingsPage from './pages/ManagerSettingsPage';
 import DashboardPage from './pages/DashboardPage';
 import Gallery from './pages/Gallery';
 import PharmaciesPage from './pages/PharmaciesPage';
+import CalendarPage from './pages/CalendarPage';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<RegistrationPage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/home" element={<HomePage />} />
-          <Route path="/gallery" element={<Gallery />} />
           <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/gallery" element={<Gallery />} />
           <Route path="/profile" element={<ProfilePage />} />
+
+          <Route path="/manager" element={<Navigate to="/manager/profile" replace />} />
           <Route path="/manager/profile" element={<ManagerProfile />} />
+          <Route path="/manager/settings" element={<ManagerSettingsPage />} />
+          <Route path="/manager/cats/:catId/vaccinations" element={<CalendarPage />} />
+
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/pharmacies" element={<PharmaciesPage />} />
         </Routes>
