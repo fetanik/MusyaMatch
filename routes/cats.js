8d55a03643449629e40b0eb8924a0cd159928fcf
Tetiana Fesiuk <fetanik28@gmail.com>
2026-04-24 01:08:10 +0300

    Implement cat CRUD and database integration

--- routes/cats.js
+++ routes/cats.js
@@ -1,8 +1,18 @@
 import { Router } from 'express';
-import { getCats } from '../controllers/catController.js';
+import {
+  getCats,
+  getCatById,
+  createCat,
+  updateCat,
+  deleteCat,
+} from '../controllers/catController.js';
 
 const router = Router();
 
 router.get('/', getCats);
+router.get('/:id', getCatById);
+router.post('/', createCat);
+router.put('/:id', updateCat);
+router.delete('/:id', deleteCat);
 
 export default router;
